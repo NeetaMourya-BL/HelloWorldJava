e7f57d56f2c5da2813e8235036edbe654ca07bb4
NeetaMourya-BL <neeta.mourya@bridgelabz.com>
2023-01-14 20:47:17 +0530

    Welcome to the Hello world program

--- src/com/bridgelabz/helloworld/HelloWorld.java
+++ src/com/bridgelabz/helloworld/HelloWorld.java
@@ -1,7 +1,12 @@
 package com.bridgelabz.helloworld;
 
+/* Welcome to hello world 
+ * program
+ */
+
 public class HelloWorld {
 	
+	// Main method
 	public static void main(String[] args) {
 		
 		System.out.println("Hello World");
